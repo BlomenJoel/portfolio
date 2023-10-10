8764b84ddbd787255788746a8cb5934201807044
Blomenjoel <joel_blomen@hotmail.com>
2023-10-10 21:04:29 +0200

    load font and type something random

--- src/lib/three/start.ts
+++ src/lib/three/start.ts
@@ -1,8 +1,10 @@
 import { browser } from '$app/environment';
 import * as THREE from 'three';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
 let renderer: THREE.WebGLRenderer;
-let cube: THREE.Mesh;
+let mesh: THREE.Mesh;
 let camera: THREE.PerspectiveCamera;
 let scene: THREE.Scene;
 const resize = () => {
@@ -25,19 +27,42 @@ if (browser) {
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color('transparent');
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-	const geometry = new THREE.BoxGeometry();
 	const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 	camera.position.z = 5;
+	const loader = new FontLoader();
+	console.log({ loader }, TextGeometry);
+	loader.load('/fonts/helvetiker_bold.typeface.json', function (font: any) {
+		const geometry = new TextGeometry('JOEL BLOMEN', {
+			font: font,
+			size: 40,
+			height: 4,
+			curveSegments: 12
+		});
+		geometry.computeBoundingBox();
 
-	cube = new THREE.Mesh(geometry, material);
-	scene.add(cube);
+		mesh = new THREE.Mesh(geometry, material);
+		mesh.position.x = -200;
+		mesh.position.z = -200;
+		mesh.position.y = 0;
+
+		mesh.rotation.x = 0;
+		mesh.rotation.y = 0;
+		scene.add(mesh);
+
+		// scene.add(geometry);
+	});
+
+	// cube = new THREE.Mesh(geometry, material);
+	// scene.add(cube);
 	window.addEventListener('resize', resize);
 }
 
 const animate = () => {
 	requestAnimationFrame(animate);
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	if (mesh) {
+		// mesh.rotation.x += 0.01;
+		// mesh.rotation.y += 0.01;
+	}
 	if (scene && camera) {
 		renderer.render(scene, camera);
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,4 +12,4 @@
 </script>
 
 <h1 class="grid h-screen place-content-center text-8xl relative z-10">Welcome to SvelteKit</h1>
-<canvas bind:this={el} class="absolute left-0 top-0" />
+<canvas bind:this={el} class="absolute left-0 top-0 w-auto" />
