5a048dff69c2e2164a849c9126660377ac23ad3f
Blomenjoel <joel_blomen@hotmail.com>
2023-10-29 20:37:05 +0100

    feat: add tram and start of about me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,9 @@
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
 	import { createScene } from '$lib/three/start';
-
+	import Name from '$lib/components/name.svelte';
+	import Tram from '$lib/components/tram.svelte';
+	import Tramstop from '$lib/components/tramstop.svelte';
 	let el: HTMLCanvasElement;
 	onMount(() => {
 		if (el && browser) {
@@ -11,5 +13,25 @@
 	});
 </script>
 
-<h1 class="grid h-screen place-content-center text-8xl relative z-10">Welcome to SvelteKit</h1>
-<canvas bind:this={el} class="absolute left-0 top-0" />
+<div class="content">
+	<div id="text-input" contenteditable="true" />
+	<div class="container" />
+</div>
+<canvas bind:this={el} class="absolute left-0 top-0 z-0" />
+<section class="h-screen">
+	<div class="z-20 relative layout--padding">
+		<Name />
+	</div>
+	<div class="-translate-y-32">
+		<div class="z-0 relative">
+			<Tram />
+		</div>
+		<div class="-translate-y-56 z-10 relative">
+			<Tramstop />
+		</div>
+	</div>
+</section>
+<section class="h-screen">
+	<h1 class="text-8xl relative z-10 mt-96 w-full text-center">ABOUT ME</h1>
+	<p class="text-center">Some text about me</p>
+</section>
