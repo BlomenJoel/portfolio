e31b797e4f4095d5b64b6abfa87347691b764c27
Blomenjoel <joel_blomen@hotmail.com>
2024-01-31 13:26:21 +0100

    feat: track what page we are now

--- src/lib/components/menu.svelte
+++ src/lib/components/menu.svelte
@@ -1,16 +1,48 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
+	import { gsap } from 'gsap';
+	import { onMount } from 'svelte';
+
+	let currentId = '';
+
+	onMount(() => {
+		if (browser) {
+			gsap.utils.toArray('section').forEach((section: any) => {
+				gsap.timeline({
+					scrollTrigger: {
+						trigger: section,
+						start: 'top 30%',
+						end: 'bottom 30%',
+						markers: true,
+						onEnter: (self: any) => {
+							currentId = self.trigger.id;
+						},
+						onEnterBack: (self: any) => {
+							currentId = self.trigger.id;
+						}
+					}
+				});
+			});
+		}
+	});
+
+	const pages = ['start', 'projects', 'about', 'contact'];
 </script>
 
-<div class="w-36 h-screen">
-	<ul class="fixed p-12 h-screen text-2xl z-10 flex flex-col justify-between">
-		<li class=""><a href="/" class="">START</a></li>
-		<li class=""><a href="/projects">PROJECTS</a></li>
-		<li class=""><a href="/about">ABOUT</a></li>
-		<li class=""><a href="/contact">CONTACT</a></li>
+<div class="absolute right-0 md:w-36 w-16 h-screen">
+	<ul class="fixed items-center h-screen text-2xl z-10 flex flex-col justify-evenly">
+		{#each pages as page}
+			<li class={currentId === page ? 'underline' : ''}>
+				<a href="/#{page}" class="uppercase">{page}</a>
+			</li>
+		{/each}
 	</ul>
 </div>
 
 <style>
+	ul {
+		width: inherit;
+	}
 	ul > li {
 		writing-mode: vertical-lr;
 		color: white;
