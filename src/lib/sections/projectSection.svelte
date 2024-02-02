4cf8aa7273d98e624f43cc0bb5be724c2af8e266
Blomenjoel <joel_blomen@hotmail.com>
2024-02-02 12:27:10 +0100

    feat: add animation to projects

--- src/lib/sections/projectSection.svelte
+++ src/lib/sections/projectSection.svelte
@@ -1,14 +1,56 @@
 <script lang="ts">
 	import HoverCursor from '$lib/components/cursor/HoverCursor.svelte';
 	import Header from '$lib/components/header.svelte';
+	import { browser } from '$app/environment';
+	import gsap from 'gsap';
+	import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+	import { onMount } from 'svelte';
+
+	let element: HTMLElement | null = null;
+	let project: HTMLElement | null = null;
+	let thirdProject: HTMLElement | null = null;
+
+	onMount(() => {
+		if (browser && element) {
+			gsap.registerPlugin(ScrollTrigger);
+
+			gsap
+				.timeline({
+					scrollTrigger: {
+						trigger: project,
+						start: 'top bottom ',
+						end: '50% 50%',
+						scrub: 2.3,
+						toggleActions: 'play pause play reverse'
+					}
+				})
+				.fromTo(
+					project,
+					{ autoAlpha: 0, y: 200, duration: 0.4 },
+					{ autoAlpha: 1, y: 0, duration: 0.4 }
+				)
+				.fromTo(
+					element,
+					{ autoAlpha: 0, y: 300, duration: 0.3 },
+					{ autoAlpha: 1, y: 0, duration: 0.3 },
+					'>'
+				)
+				.fromTo(
+					thirdProject,
+					{ autoAlpha: 0, y: 300, duration: 0.6 },
+					{ autoAlpha: 1, y: 0, duration: 0.6 },
+					'>'
+				);
+		}
+	});
 </script>
 
 <section class="p-8 md:p-24 md:pr-36 pr-16 bg-main flex flex-col items-center" id="projects">
 	<Header header="SELECTED PROJECTS" />
-	<ul class="flex flex-row gap-4 flex-wrap justify-center w-full">
-		<div class="ml-auto w-[40rem]">
+	<ul class="flex flex-row gap-12 mt-24 flex-wrap justify-center w-full">
+		<div class="ml-auto w-[40rem]" bind:this={project}>
 			<HoverCursor colorTheme="dark" cursorText="CHECK IT OUT" link="https://raformat.se/sv/">
-				<li class="p-12 flex md:flex-row flex-col gap-4 items-center">
+				<li class=" flex md:flex-row flex-col gap-4 items-center">
 					<img src="/raformat.png" alt="raformat webpage" class="md:w-2/3" />
 					<div class="md:w-1/3 w-full text-white">
 						<h3 class=" text-3xl">RÅFORMAT</h3>
@@ -19,13 +61,13 @@
 				</li>
 			</HoverCursor>
 		</div>
-		<div class="mr-auto w-[40rem]">
+		<div class="mr-auto w-[40rem]" bind:this={element}>
 			<HoverCursor
 				colorTheme="light"
 				cursorText="CHECK IT OUT"
 				link="https://www.belidlightinggroup.com/sv-se"
 			>
-				<li class="p-12 flex md:flex-row flex-col gap-4 items-center">
+				<li class=" flex md:flex-row flex-col gap-4 items-center">
 					<img src="/blg.png" alt="Belid Lightning Group webpage" class="md:w-2/3" />
 					<div class="md:w-1/3 w-full text-white">
 						<h3 class="text-3xl">BELID LIGHTNING GROUP</h3>
@@ -36,9 +78,9 @@
 				</li>
 			</HoverCursor>
 		</div>
-		<div class="mx-auto w-[40rem]">
+		<div class="mx-auto w-[40rem]" bind:this={thirdProject}>
 			<HoverCursor colorTheme="dark" cursorText="CHECK IT OUT" link="https://queenslab.co/en/">
-				<li class="p-12 flex md:flex-row flex-col gap-4 items-center">
+				<li class=" flex md:flex-row flex-col gap-4 items-center">
 					<img src="/queenslab.png" alt="QueensLab webpage" class="md:w-2/3" />
 					<div class="md:w-1/3 w-full text-white">
 						<h3 class="text-3xl">QUEENSLAB</h3>
