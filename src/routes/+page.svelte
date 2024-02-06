0e4d1036976bd1c16335b37e96b305b91f07d472
Blomenjoel <joel_blomen@hotmail.com>
2024-02-06 11:49:52 +0100

    fix: meta data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,16 @@
 	import Contact from '$lib/sections/contactSection.svelte';
 </script>
 
+<svelte:head>
+	<title>🚧Joels portfolio🚧</title>
+	<meta property="og:title" content="🚧Joels portfolio🚧" />
+	<meta property="twitter:title" content="🚧Joels portfolio🚧" />
+	<meta name="description" content="Joel Blomén portfolio" />
+	<meta property="og:description" content="Joel Blomén portfolio" />
+	<meta property="twitter:description" content="Joel Blomén portfolio" />
+	<meta property="og:image" content="%sveltekit.assets%/favicon.png" />
+	<meta property="twitter:image" content="%sveltekit.assets%/favicon.png" />
+</svelte:head>
 <Face />
 <Projects />
 <About />
