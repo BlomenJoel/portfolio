fcc04caac23d1701e5cfd8f9518bb0fd68c32cf3
Joel <joel_blomen@hotmail.com>
2024-06-27 16:25:18 +1000

    feat: experiment with mouseflow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,4 +11,14 @@
 		<slot />
 	</div>
 	<Menu />
+	<script type="text/javascript">
+		window._mfq = window._mfq || [];
+		(function () {
+			var mf = document.createElement('script');
+			mf.type = 'text/javascript';
+			mf.defer = true;
+			mf.src = '//cdn.mouseflow.com/projects/3db6816d-61a0-48ba-ae4b-240164f61c57.js';
+			document.getElementsByTagName('head')[0].appendChild(mf);
+		})();
+	</script>
 </div>
